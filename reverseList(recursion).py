da94bb222be3a7e4260adc7d677443afdebf657a
Raphael Adjetey Adjei <100845372+RAAdjei@users.noreply.github.com>
2023-10-27 09:39:01 +0000

    Create reverseList(recursion).py

---
+++ reverseList(recursion).py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not head:
+            return
+        if head and head.next == None:
+            return head
+         
+
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return newHead
+
+
+
+
+
+
+
+
+
