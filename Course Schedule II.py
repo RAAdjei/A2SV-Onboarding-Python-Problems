aad5b1d2e530933589bb1a65dc333ea5fdc1f58d
Raphael Adjetey Adjei <100845372+RAAdjei@users.noreply.github.com>
2023-12-20 20:59:22 +0000

    Update Course Schedule II.py

--- Course Schedule II.py
+++ Course Schedule II.py
@@ -1,17 +1,17 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         queue = deque()
-        count = {i:0 for i in range(numCourses)}
+        incoming = {i:0 for i in range(numCourses)}
         res = []
 
         adj = defaultdict(list)
         
         for key, val in prerequisites:
             adj[val].append(key)
-            count[key] += 1
+            incoming[key] += 1
 
-        for i in count:
-            if count[i] == 0:
+        for i in incoming:
+            if incoming[i] == 0:
                 queue.append(i)        
         
         while queue:
@@ -19,8 +19,8 @@ class Solution:
             res.append(cur_course)
 
             for neighbour in adj[cur_course]:
-                count[neighbour] -= 1
-                if count[neighbour] == 0:
+                incoming[neighbour] -= 1
+                if incoming[neighbour] == 0:
                     queue.append(neighbour)
 
             
