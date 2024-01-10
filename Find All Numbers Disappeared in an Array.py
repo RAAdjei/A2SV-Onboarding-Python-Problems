d861a782c2ed56c52ab8ae07bf3334e4aa4b72db
Raphael Adjetey Adjei <100845372+RAAdjei@users.noreply.github.com>
2024-01-10 13:15:48 +0000

    Create Find All Numbers Disappeared in an Array.py

---
+++ Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,15 @@
+output = []
+        n = len(nums)
+        i = 0
+
+        while i < n:
+            idx = nums[i]-1
+            if nums[i] != nums[i]+1 and nums[idx] != nums[i]:
+                nums[idx], nums[i] = nums[i], nums[idx]
+
+            else:
+                i += 1
+
+        output = [i+1 for i in range(n) if nums[i] != i+1]
+
+        return output
